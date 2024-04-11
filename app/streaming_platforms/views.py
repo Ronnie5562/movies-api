8ca463c74ff55194edcfcbb145b726b03b2fa444
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-04-11 17:15:40 +0200

    Created: Built out the views

--- app/streaming_platforms/views.py
+++ app/streaming_platforms/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render # noqa
+"""
+Views for the streaming platform API.
+"""
+from rest_framework import generics, mixins
+from streaming_platforms.throttling import (
+    UserPlatformViewThrottle,
+    AnonPlatformViewThrottle
+)
+from streaming_platforms.models import Platform
+from streaming_platforms.serializers import PlatformSerializer
 
-# Create your views here.
+
+class PlatformsView(
+                    mixins.ListModelMixin,
+                    mixins.RetrieveModelMixin,
+                    generics.GenericAPIView):
+    """
+    List all streaming platforms or retrieve a single platform.
+    """
+    queryset = Platform.objects.all().order_by('-id')
+    serializer_class = PlatformSerializer
+    lookup_field = 'id'
+    throttle_classes = [UserPlatformViewThrottle, AnonPlatformViewThrottle]
+
+    def get(self, request, *args, **kwargs):
+        """
+        Get the either the streaming platforms list \
+        or a single streaming platform
+        """
+        if kwargs.get('id'):
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
