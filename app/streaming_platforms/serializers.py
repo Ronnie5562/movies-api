04748924fbb5481d2ffdc5d380d6bb9fb99d7d0f
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-04-11 17:12:12 +0200

    Created: Created the serializers for the platforms APIs.

---
+++ app/streaming_platforms/serializers.py
@@ -0,0 +1,14 @@
+"""
+Stream Platforms API Serializers
+"""
+from rest_framework import serializers
+from streaming_platforms.models import Platform
+
+class PlatformSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Stream Platform object
+    """
+    class Meta:
+        model = Platform
+        fields = ["name", "website", "about"]
+        read_only_fields = ['id']
