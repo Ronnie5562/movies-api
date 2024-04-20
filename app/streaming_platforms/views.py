31960082866f8fc4792ce7a54d888ef9dfca2e2d
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-04-20 17:53:52 +0200

    Updated the stream platform API Views

--- app/streaming_platforms/views.py
+++ app/streaming_platforms/views.py
@@ -1,7 +1,7 @@
 """
 Views for the streaming platform API.
 """
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, permissions
 from streaming_platforms.throttling import (
     UserPlatformViewThrottle,
     AnonPlatformViewThrottle
@@ -17,10 +17,19 @@ class PlatformsView(generics.GenericAPIView):
     queryset = Platform.objects.all().order_by('-id')
     serializer_class = PlatformSerializer
     lookup_field = 'id'
-    throttle_classes = [AnonPlatformViewThrottle, UserPlatformViewThrottle]
+    throttle_classes = [AnonPlatformViewThrottle,
+                        UserPlatformViewThrottle]
+
+    def get_permissions(self):
+        if self.request.method not in permissions.SAFE_METHODS:
+            return [permissions.IsAdminUser]
+        else:
+            return super().get_permissions()
 
 
-class PlatformListView(mixins.ListModelMixin, PlatformsView):
+class PlatformListView(mixins.ListModelMixin,
+                       mixins.CreateModelMixin,
+                       PlatformsView):
     """
     List all streaming platforms.
     """
@@ -30,8 +39,15 @@ class PlatformListView(mixins.ListModelMixin, PlatformsView):
         """
         return self.list(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        """Give out an award to an actor or director"""
+        return self.create(request, *args, **kwargs)
 
-class PlatformRetrieveView(mixins.RetrieveModelMixin, PlatformsView):
+
+class PlatformRetrieveView(mixins.RetrieveModelMixin,
+                           mixins.UpdateModelMixin,
+                           mixins.DestroyModelMixin,
+                           PlatformsView):
     """
     Retrieve a single streaming platform.
     """
@@ -40,3 +56,15 @@ class PlatformRetrieveView(mixins.RetrieveModelMixin, PlatformsView):
         Get a single streaming platform
         """
         return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        """Update the details of a single platform"""
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        """Update the details of a single platform"""
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        """Delete a single platform"""
+        return self.destroy(request, *args, **kwargs)
